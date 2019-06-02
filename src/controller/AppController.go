af45ef51ea297bc8e5d62b276c05a9c7004fac3f
Diego CAO <cao.trung.thu@gmail.com>
2019-06-02 20:40:26 +0700

    add login api

--- src/controller/AppController.go
+++ src/controller/AppController.go
@@ -1,11 +1,23 @@
 package controller
 
 import (
+	"encoding/json"
 	"net/http"
+	u "rest-api-golang/src/utils"
+	"rest-api-golang/src/models"
 )
 
 var Login = func(w http.ResponseWriter, r *http.Request) {
+	user := &models.User{}
+	err := json.NewDecoder(r.Body).Decode(user) 
+	if err != nil {
+		u.Respond(w, u.Message(false, "Invalid request"))
+		return
+	}
 
+	resp := u.Message(true, "Successful")
+	resp["data"] = user
+	u.Respond(w, resp)
 }
 
 var RefreshToken = func(w http.ResponseWriter, r *http.Request) {
