7e5c1f07bccc7febdf511e53f7844e6c915041c2
Diego CAO <cao.trung.thu@gmail.com>
2019-06-02 16:56:18 +0700

    update apis

--- src/controller/AppController.go
+++ src/controller/AppController.go
@@ -1,1 +1,17 @@
 package controller
+
+import (
+	"net/http"
+)
+
+var Login = func(w http.ResponseWriter, r *http.Request) {
+
+}
+
+var RefreshToken = func(w http.ResponseWriter, r *http.Request) {
+
+}
+
+var GetUser = func(w http.ResponseWriter, r *http.Request) {
+
+}
--- src/routes/AppRoute.go
+++ src/routes/AppRoute.go
@@ -3,12 +3,18 @@ package routes
 import (
 	"fmt"
 	"net/http"
+	"rest-api-golang/src/controller"
 
 	"github.com/gorilla/mux"
 )
 
 var SetupServer = func(appPort string) {
 	var router = mux.NewRouter()
+
+	router.HandleFunc("/api/login", controller.Login).Methods("POST")
+	router.HandleFunc("/api/refreshToken", controller.RefreshToken).Methods("POST")
+	router.HandleFunc("/api/getUser", controller.GetUser).Methods("GET")
+
 	fmt.Println(appPort)
 	err := http.ListenAndServe(":"+appPort, router)
 	if err != nil {
